5a6f8ab3f6ab33ef2a97d5c9d0399ee197df3fe2
Arne Schwarck <arneschwarck@gmail.com>
2020-03-03 21:53:48 +0100

    try bind id

--- selfdrive/crash.py
+++ selfdrive/crash.py
@@ -40,11 +40,11 @@ else:
     cloudlog.error("crash", exc_info=kwargs.get('exc_info', 1))
     
   def capture_warning(warning_string):
-    client.user_context('username': uniqueID)
+    client.user_context(id= uniqueID)
     client.captureMessage(warning_string, level='warning')
   
   def capture_info(info_string):
-    client.user_context('username': uniqueID)
+    client.user_context(id= uniqueID)
     client.captureMessage(info_string, level='info')
     
   def bind_user(**kwargs):
