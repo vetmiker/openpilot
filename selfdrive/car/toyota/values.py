285100d55589a88a7d2508331783ae207e6d85ed
Arne Schwarck <arneschwarck@gmail.com>
2020-03-03 08:11:37 +0100

    Add CAMRY_TSS2

--- selfdrive/car/toyota/values.py
+++ selfdrive/car/toyota/values.py
@@ -20,6 +20,7 @@ class CAR:
   CHR = "TOYOTA C-HR 2018"
   CHRH = "TOYOTA C-HR HYBRID 2018"
   CAMRY = "TOYOTA CAMRY 2018"
+  CAMRY_TSS2 = "TOYOTA CAMRY 2019"
   CAMRYH = "TOYOTA CAMRY HYBRID 2018"
   HIGHLANDER = "TOYOTA HIGHLANDER 2017"
   HIGHLANDER_TSS2 = "TOYOTA HIGHLANDER 2020"
@@ -684,6 +685,7 @@ DBC = {
   CAR.HIGHLANDER_TSS2: dbc_dict('toyota_nodsu_pt_generated', 'toyota_tss2_adas'),
   CAR.HIGHLANDERH: dbc_dict('toyota_highlander_hybrid_2018_pt_generated', 'toyota_adas'),
   CAR.AVALON: dbc_dict('toyota_avalon_2017_pt_generated', 'toyota_adas'),
+  CAR.CAMRY_TSS2: dbc_dict('toyota_nodsu_pt_generated', 'toyota_tss2_adas'),
   CAR.RAV4_TSS2: dbc_dict('toyota_nodsu_pt_generated', 'toyota_tss2_adas'),
   CAR.RAV4H_TSS2: dbc_dict('toyota_nodsu_hybrid_pt_generated', 'toyota_tss2_adas'),
   CAR.COROLLA_TSS2: dbc_dict('toyota_nodsu_pt_generated', 'toyota_tss2_adas'),
