240862b063c042378f9fbe33d853871e6ad99a2f
Arne Schwarck <arneschwarck@gmail.com>
2020-03-09 12:45:05 +0100

    Only add a 5th of the TR mod to cost

--- selfdrive/controls/lib/long_mpc.py
+++ selfdrive/controls/lib/long_mpc.py
@@ -108,7 +108,7 @@ class LongitudinalMpc():
       cost = 0.1
       cost = cost * min(max(1.0 , (6.0 - vEgo)),5.0) 
     if self.TR_Mod > 0:
-      cost = cost + self.TR_Mod
+      cost = cost + self.TR_Mod/5.0
     if self.last_cost != cost:
       self.libmpc.change_tr(MPC_COST_LONG.TTC, cost, MPC_COST_LONG.ACCELERATION, MPC_COST_LONG.JERK)
       self.last_cost = cost
