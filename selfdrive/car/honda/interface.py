462dc7a43aaf618c507d8b8aa8f77bcd0fa6dead
Arne Schwarck <arneschwarck@gmail.com>
2020-03-11 16:44:07 +0100

    revert Honda fix

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -379,9 +379,10 @@ class CarInterface(CarInterfaceBase):
     # ******************* do can recv *******************
     self.cp.update_strings(can_strings)
     self.cp_cam.update_strings(can_strings)
+    self.CS.update(self.cp, self.cp_cam)
 
     # create message
-    ret = self.CS.update(self.cp, self.cp_cam)
+    ret = car.CarState.new_message()
     ret_arne182 = arne182.CarStateArne182.new_message()
 
     ret.canValid = self.cp.can_valid and self.cp_cam.can_valid
