43f6b6d6e3002a4d2c388efac305c880fab7f76a
Arne Schwarck <arneschwarck@gmail.com>
2020-03-20 21:12:24 +0100

    comma renaming things again

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -120,7 +120,7 @@ class CarController():
       self.last_fault_frame = frame
 
     # Cut steering for 2s after fault
-    if (frame - self.last_fault_frame < 200) or abs(CS.out.angle_steers) > 100 or abs(CS.out.angle_steers_rate) > 100:
+    if (frame - self.last_fault_frame < 200) or abs(CS.out.steeringRate) > 100 or abs(CS.out.steeringAngle) > 100:
       new_steer = 0
       apply_steer_req = 0
     else:
