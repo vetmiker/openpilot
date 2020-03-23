cacbd8967135f24fbfbad0c36cf0c962e51f5ede
Arne Schwarck <arneschwarck@gmail.com>
2020-03-23 17:54:06 +0100

    Allow fcw with unknown ecu

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -206,7 +206,7 @@ class CarController():
     if (frame % 100 == 0 or send_ui) and Ecu.fwdCamera in self.fake_ecus:
       can_sends.append(create_ui_command(self.packer, steer, pcm_cancel_cmd, left_line, right_line, left_lane_depart, right_lane_depart))
 
-    if frame % 100 == 0 and Ecu.dsu in self.fake_ecus:
+    if frame % 100 == 0 and (Ecu.dsu in self.fake_ecus or Ecu.unknown in self.fake_ecus):
       can_sends.append(create_fcw_command(self.packer, fcw))
 
     #*** static msgs ***
