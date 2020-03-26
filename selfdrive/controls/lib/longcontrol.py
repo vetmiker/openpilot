87287f2ae2ebc2c29f44168d9fc07550909dbff0
Arne Schwarck <arneschwarck@gmail.com>
2020-03-26 10:33:33 +0100

    When changing CP values reset the PID to the current speed

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -125,16 +125,22 @@ class LongControl():
           self.pid._k_i = (CP.longitudinalTuning.kiBP, [x * 0 for x in CP.longitudinalTuning.kiV])
           self.pid.i = 0.0
           self.pid.k_f=1.0
+          self.v_pid = v_ego
+          self.pid.reset()
         if self.lastdecelForTurn and not decelForTurn:
           self.lastdecelForTurn = False
           self.pid._k_p = (CP.longitudinalTuning.kpBP, CP.longitudinalTuning.kpV)
           self.pid._k_i = (CP.longitudinalTuning.kiBP, CP.longitudinalTuning.kiV)
           self.pid.k_f=1.0
+          self.v_pid = v_ego
+          self.pid.reset()
       else:
         self.lastdecelForTurn = False
         self.pid._k_p = (CP.longitudinalTuning.kpBP, [x * 1 for x in CP.longitudinalTuning.kpV])
         self.pid._k_i = (CP.longitudinalTuning.kiBP, [x * 1 for x in CP.longitudinalTuning.kiV])
         self.pid.k_f=1.0
+        self.v_pid = v_ego
+        self.pid.reset()
       
       output_gb = self.pid.update(self.v_pid, v_ego_pid, speed=v_ego_pid, deadzone=deadzone, feedforward=a_target, freeze_integrator=prevent_overshoot)
 
