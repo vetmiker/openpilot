07aa11007c9c9404e7a4e1a6deae1ebe0e68d024
Arne Schwarck <arneschwarck@gmail.com>
2020-04-04 10:58:27 +0200

    revert gernby for pid

--- selfdrive/controls/lib/pathplanner.py
+++ selfdrive/controls/lib/pathplanner.py
@@ -202,8 +202,8 @@ class PathPlanner():
     # account for actuation delay
     self.cur_state = calc_states_after_delay(self.cur_state, v_ego, angle_steers - angle_offset, curvature_factor, VM.sR, CP.steerActuatorDelay)
 
-    if abs(angle_steers - angle_offset) > 4 and CP.lateralTuning.which() == 'pid':
-      self.cur_state[0].delta = math.radians(angle_steers - angle_offset) / VM.sR
+    #if abs(angle_steers - angle_offset) > 4 and CP.lateralTuning.which() == 'pid': #check if this causes laggy steering
+    #  self.cur_state[0].delta = math.radians(angle_steers - angle_offset) / VM.sR
 
     v_ego_mpc = max(v_ego, 5.0)  # avoid mpc roughness due to low speed
     self.libmpc.run_mpc(self.cur_state, self.mpc_solution,
