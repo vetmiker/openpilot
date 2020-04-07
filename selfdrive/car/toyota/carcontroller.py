6724644d7c392c0d3ff6d903d61dcebfbf6f7d96
Arne Schwarck <arneschwarck@gmail.com>
2020-04-07 21:12:22 +0200

    Add Prius back to fix for steeringAngle

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -162,7 +162,7 @@ class CarController():
       self.last_fault_frame = frame
 
     # Cut steering for 1s after fault
-    if (frame - self.last_fault_frame < 100) or abs(CS.out.steeringRate) > 100 or (abs(CS.out.steeringAngle) > 100 and CS.CP.carFingerprint == CAR.RAV4H):
+    if (frame - self.last_fault_frame < 100) or abs(CS.out.steeringRate) > 100 or (abs(CS.out.steeringAngle) > 100 and CS.CP.carFingerprint in [CAR.RAV4H, CAR.PRIUS]):
       new_steer = 0
       apply_steer_req = 0
     else:
