d4eade9caf25b104d9c5b5c85709f2258beb467e
Arne Schwarck <arneschwarck@gmail.com>
2020-04-13 17:26:16 +0200

    Do not forget to pray!
    Add slogan and make code safer!

--- selfdrive/controls/lib/alerts.py
+++ selfdrive/controls/lib/alerts.py
@@ -202,9 +202,9 @@ ALERTS = [
   Alert(
       "startup",
       "Be ready to take over at any time",
-      "Always keep hands on wheel and eyes on road",
+      "Do not forget to PRAY!",
       AlertStatus.normal, AlertSize.mid,
-      Priority.LOWER, VisualAlert.none, AudibleAlert.none, 0., 0., 5.),
+      Priority.LOWER, VisualAlert.none, AudibleAlert.none, 0., 0., 8.),
 
   Alert(
       "startupMaster",
