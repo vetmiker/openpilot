7c4722af481e504e015abe16e3eeb63cba1c9260
Arne Schwarck <arneschwarck@gmail.com>
2020-04-14 09:58:56 +0200

    Remove sleep

--- selfdrive/trafficd/traffic_manager.py
+++ selfdrive/trafficd/traffic_manager.py
@@ -15,7 +15,7 @@ class Traffic:
     self.recurrent_length = 1.0  # in seconds, how far back to factor into current prediction
     self.min_preds = int(round(self.recurrent_length / self.model_rate))
     self.last_pred_weight = 5.  # places nx weight on most recent prediction
-    self.trafficd_timeout = 10.0  # in seconds, how long to wait before realizing trafficd is dead
+    self.trafficd_timeout = 5.0  # in seconds, how long to wait before realizing trafficd is dead
 
     self.past_preds = []
     self.weights = np.linspace(1, self.last_pred_weight, self.min_preds)
@@ -84,7 +84,6 @@ class Traffic:
 
 
 def main():
-  time.sleep(5)
   traffic = Traffic()
   traffic.start()
 
