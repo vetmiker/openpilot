d67e14ffd5f938b210fc382cbfcb99e87ed382cb
Arne Schwarck <arneschwarck@gmail.com>
2020-05-03 11:16:51 +0200

    Update safety_declarations.h

--- panda/board/safety_declarations.h
+++ panda/board/safety_declarations.h
@@ -40,6 +40,7 @@ int safety_tx_hook(CAN_FIFOMailBox_TypeDef *to_send);
 int safety_tx_lin_hook(int lin_num, uint8_t *data, int len);
 uint32_t get_ts_elapsed(uint32_t ts, uint32_t ts_last);
 int to_signed(int d, int bits);
+bool get_int16_flag(int16_t param, uint16_t mask);
 void update_sample(struct sample_t *sample, int sample_new);
 bool max_limit_check(int val, const int MAX, const int MIN);
 bool dist_to_meas_check(int val, int val_last, struct sample_t *val_meas,
