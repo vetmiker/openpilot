6f9b7408b50ace21034306ec84ea5b07fee067df
Arne Schwarck <arneschwarck@gmail.com>
2020-05-04 21:10:08 +0200

    Update pathplanner.py

--- selfdrive/controls/lib/pathplanner.py
+++ selfdrive/controls/lib/pathplanner.py
@@ -41,7 +41,7 @@ DESIRES = {
 
 
 def calc_states_after_delay(states, v_ego, steer_angle, curvature_factor, steer_ratio, delay):
-  states[0].x = v_ego * delay
+  states[0].x = max(v_ego * delay, 0.0)
   states[0].psi = v_ego * curvature_factor * math.radians(steer_angle) / steer_ratio * delay
   return states
 
