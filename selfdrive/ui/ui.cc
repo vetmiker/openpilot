87c9f4e1f14c387536eac343789181ad8a9ee81d
Shane <shane@smiskol.com>
2020-05-28 18:56:29 -0500

    allow for auto profile

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -142,7 +142,7 @@ static bool handle_df_touch(UIState *s, int touch_x, int touch_y) {
    if (touch_x >= 1212 && touch_x <= 1310 && touch_y >= 902 && touch_y <= 1013) {
       s->scene.uilayout_sidebarcollapsed = true;  // collapse sidebar when tapping df button
       s->scene.dfButtonStatus++;
-      if (s->scene.dfButtonStatus > 2) {
+      if (s->scene.dfButtonStatus > 3) {
         s->scene.dfButtonStatus = 0;
       }
       send_df(s, s->scene.dfButtonStatus);
