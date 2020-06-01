cb7ca2bd52514facc6424e726a8462503603de42
Shane <shane@smiskol.com>
2020-06-01 07:05:02 -0500

    add new username param

--- selfdrive/crash.py
+++ selfdrive/crash.py
@@ -8,7 +8,6 @@ from common.params import Params
 from selfdrive.version import version, dirty, origin, branch
 from common.op_params import opParams
 op_params = opParams()
-uniqueID = op_params.get('uniqueID', None)
 
 from selfdrive.swaglog import cloudlog
 from common.android import ANDROID
@@ -34,8 +33,11 @@ else:
     ipaddress = requests.get('https://checkip.amazonaws.com/').text.strip()
   except:
     ipaddress = "255.255.255.255"
-  error_tags = {'dirty': dirty, 'username': uniqueID, 'dongle_id': dongle_id, 'branch': branch, 'remote': origin}
-  
+  error_tags = {'dirty': dirty, 'dongle_id': dongle_id, 'branch': branch, 'remote': origin}
+  username = op_params.get('username', None)
+  if username is not None and isinstance(username, str):
+    error_tags['username'] = username
+
   client = Client('https://137e8e621f114f858f4c392c52e18c6d:8aba82f49af040c8aac45e95a8484970@sentry.io/1404547',
                   install_sys_hook=False, transport=HTTPTransport, release=version, tags=error_tags)
 
