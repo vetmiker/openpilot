e47b73bfc7bb5618ba60c170d4742fffd9aecada
Shane Smiskol <shane@smiskol.com>
2020-06-19 19:36:09 -0500

    add new auto-df model, fix global df mod (#634)
    * add new auto-df model
    
    * add min_TR and fix global_df_mod weird behavior. NEED TO TEST!
    
    * why did i add this?
    
    * Travis fix
    
    Co-authored-by: Arne Schwarck <arneschwarck@gmail.com>

--- selfdrive/controls/lib/dynamic_follow/auto_df.py
+++ selfdrive/controls/lib/dynamic_follow/auto_df.py
@@ -2,13 +2,11 @@
   Generated using Konverter: https://github.com/ShaneSmiskol/Konverter
 """
 
+from common.basedir import BASEDIR
 import numpy as np
 
-
-def load_weights():
-  global w, b
-  wb = np.load('/data/openpilot/selfdrive/controls/lib/dynamic_follow/auto_df_weights.npz', allow_pickle=True)
-  w, b = wb['wb']
+wb = np.load(BASEDIR + '/selfdrive/controls/lib/dynamic_follow/auto_df_weights.npz', allow_pickle=True)
+w, b = wb['wb']
 
 def softmax(x):
   return np.exp(x) / np.sum(np.exp(x), axis=0)
@@ -19,7 +17,5 @@ def predict(x):
   l1 = np.dot(l0, w[1]) + b[1]
   l1 = np.maximum(0, l1)
   l2 = np.dot(l1, w[2]) + b[2]
-  l2 = np.maximum(0, l2)
-  l3 = np.dot(l2, w[3]) + b[3]
-  l3 = softmax(l3)
-  return l3
+  l2 = softmax(l2)
+  return l2
