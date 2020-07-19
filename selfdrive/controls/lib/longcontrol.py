c2db8d6adca2d9608989437889d9d28d47099308
Arne Schwarck <arneschwarck@gmail.com>
2020-07-19 20:09:44 +0200

    Update longcontrol.py

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -25,13 +25,8 @@ RATE = 100.0
 def long_control_state_trans(active, long_control_state, v_ego, v_target, v_pid,
                              output_gb, brake_pressed, cruise_standstill, stop):
   """Update longitudinal control state machine"""
-<<<<<<< HEAD
   stopping_condition = stop or (v_ego < 2.0 and cruise_standstill) or \
                        (v_ego < STOPPING_EGO_SPEED and \
-=======
-  stopping_condition = (v_ego < 2.0 and cruise_standstill) or \
-                       (v_ego < STOPPING_EGO_SPEED and
->>>>>>> b205dd6954ad6d795fc04d66e0150675b4fae28d
                         ((v_pid < STOPPING_TARGET_SPEED and v_target < STOPPING_TARGET_SPEED) or
                         brake_pressed))
 
@@ -84,7 +79,6 @@ class LongControl():
     self.pid.reset()
     self.v_pid = v_pid
 
-<<<<<<< HEAD
   def dynamic_gas(self, v_ego, gas_interceptor, gas_button_status):
     dynamic = False
     if gas_interceptor:
@@ -125,20 +119,16 @@ class LongControl():
     max_return = 1.0
     return round(max(min(accel, max_return), min_return), 5)  # ensure we return a value between range
 
-  def update(self, active, v_ego, gas_pressed, brake_pressed, standstill, cruise_standstill, v_cruise, v_target, v_target_future, a_target, CP, hasLead, dRel, decelForTurn, longitudinalPlanSource, gas_button_status):
-=======
-  def update(self, active, CS, v_target, v_target_future, a_target, CP):
->>>>>>> b205dd6954ad6d795fc04d66e0150675b4fae28d
+  def update(self, active, CS, v_target, v_target_future, a_target, CP, hasLead, dRel, decelForTurn, longitudinalPlanSource, gas_button_status):
     """Update longitudinal control. This updates the state machine and runs a PID loop"""
     try:
       gas_interceptor = CP.enableGasInterceptor
     except AttributeError:
       gas_interceptor = False
     # Actuation limits
-<<<<<<< HEAD
-    #gas_max = interp(v_ego, CP.gasMaxBP, CP.gasMaxV)
-    gas_max = self.dynamic_gas(v_ego, gas_interceptor, gas_button_status)
-    brake_max = interp(v_ego, CP.brakeMaxBP, CP.brakeMaxV)
+    #gas_max = interp(CS.vEgo, CP.gasMaxBP, CP.gasMaxV)
+    gas_max = self.dynamic_gas(CS.vEgo, gas_interceptor, gas_button_status)
+    brake_max = interp(CS.vEgo, CP.brakeMaxBP, CP.brakeMaxV)
 
     # Update state machine
     output_gb = self.last_output_gb
@@ -146,19 +136,10 @@ class LongControl():
       stop = True if dRel < 4.0 else False
     else:
       stop = False
-    self.long_control_state = long_control_state_trans(active, self.long_control_state, v_ego,
-                                                       v_target_future, self.v_pid, output_gb,
-                                                       brake_pressed, cruise_standstill, stop)
-=======
-    gas_max = interp(CS.vEgo, CP.gasMaxBP, CP.gasMaxV)
-    brake_max = interp(CS.vEgo, CP.brakeMaxBP, CP.brakeMaxV)
-
-    # Update state machine
-    output_gb = self.last_output_gb
     self.long_control_state = long_control_state_trans(active, self.long_control_state, CS.vEgo,
                                                        v_target_future, self.v_pid, output_gb,
-                                                       CS.brakePressed, CS.cruiseState.standstill)
->>>>>>> b205dd6954ad6d795fc04d66e0150675b4fae28d
+                                                       CS.brake_pressed, CS.cruise_standstill, stop)
+
 
     v_ego_pid = max(CS.vEgo, MIN_CAN_SPEED)  # Without this we get jumps, CAN bus reports 0 when speed < 0.3
 
@@ -195,18 +176,18 @@ class LongControl():
           self.pid._k_i = (CP.longitudinalTuning.kiBP, [x * 0 for x in CP.longitudinalTuning.kiV])
           self.pid.i = 0.0
           self.pid.k_f=1.0
-          self.v_pid = v_ego
+          self.v_pid = CS.vEgo
           self.pid.reset()
         if self.lastdecelForTurn and not decelForTurn:
           self.lastdecelForTurn = False
           self.pid._k_p = (CP.longitudinalTuning.kpBP, CP.longitudinalTuning.kpV)
           self.pid._k_i = (CP.longitudinalTuning.kiBP, CP.longitudinalTuning.kiV)
           self.pid.k_f=1.0
-          self.v_pid = v_ego
+          self.v_pid = CS.vEgo
           self.pid.reset()
       else:
         if self.lastdecelForTurn:
-          self.v_pid = v_ego
+          self.v_pid = CS.vEgo
           self.pid.reset()
         self.lastdecelForTurn = False
         self.pid._k_p = (CP.longitudinalTuning.kpBP, [x * 1 for x in CP.longitudinalTuning.kpV])
@@ -222,16 +203,11 @@ class LongControl():
     # Intention is to stop, switch to a different brake control until we stop
     elif self.long_control_state == LongCtrlState.stopping:
       # Keep applying brakes until the car is stopped
-<<<<<<< HEAD
       factor = 1
       if hasLead:
         factor = interp(dRel,[2.0,3.0,4.0,5.0,6.0,7.0,8.0], [5.0,2.5,1.0,0.5,0.25,0.05,0.0])
-      if not standstill or output_gb > -BRAKE_STOPPING_TARGET:
-        output_gb -= STOPPING_BRAKE_RATE / RATE * factor
-=======
       if not CS.standstill or output_gb > -BRAKE_STOPPING_TARGET:
-        output_gb -= STOPPING_BRAKE_RATE / RATE
->>>>>>> b205dd6954ad6d795fc04d66e0150675b4fae28d
+        output_gb -= STOPPING_BRAKE_RATE / RATE * factor
       output_gb = clip(output_gb, -brake_max, gas_max)
 
       self.v_pid = CS.vEgo
@@ -243,13 +219,8 @@ class LongControl():
       if hasLead:
         factor = interp(dRel,[0.0,2.0,4.0,6.0], [0.0,0.5,1.0,2.0])
       if output_gb < -0.2:
-<<<<<<< HEAD
         output_gb += STARTING_BRAKE_RATE / RATE * factor
-      self.v_pid = v_ego
-=======
-        output_gb += STARTING_BRAKE_RATE / RATE
       self.v_pid = CS.vEgo
->>>>>>> b205dd6954ad6d795fc04d66e0150675b4fae28d
       self.pid.reset()
 
     self.last_output_gb = output_gb
