afe982705f5213fbdaa62947485392249aa3199d
Arne Schwarck <arneschwarck@gmail.com>
2020-07-20 18:05:32 +0200

    Update boardd_setup.py

--- selfdrive/boardd/boardd_setup.py
+++ selfdrive/boardd/boardd_setup.py
@@ -10,24 +10,7 @@ import os
 PHONELIBS = os.path.join(BASEDIR, 'phonelibs')
 
 ARCH = subprocess.check_output(["uname", "-m"], encoding='utf8').rstrip()
-<<<<<<< HEAD
-
-if ARCH == "x86_64":
-  if platform.system() == "Darwin":
-    libraries = ['can_list_to_can_capnp', 'capnp', 'kj']
-    ARCH_DIR = 'mac'
-  else:
-    libraries = [':libcan_list_to_can_capnp.a', ':libcapnp.a', ':libkj.a']
-    ARCH_DIR = 'x64'
-else:
-  libraries = [':libcan_list_to_can_capnp.a', 'capnp', 'kj']
-  if os.path.isdir("/system"):
-    ARCH_DIR = 'aarch64'
-  else:
-    ARCH_DIR = 'larch64'
-=======
 libraries = ['can_list_to_can_capnp', 'capnp', 'kj']
->>>>>>> b205dd6954ad6d795fc04d66e0150675b4fae28d
 
 setup(name='Boardd API Implementation',
       cmdclass={'build_ext': BuildExtWithoutPlatformSuffix},
