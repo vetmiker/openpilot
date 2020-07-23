f468009da6bf0599d0c7e9d4a231e8ed1a6fdbf4
Arne Schwarck <arneschwarck@gmail.com>
2020-07-23 13:48:13 +0200

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -169,7 +169,6 @@ class CarInterface(CarInterfaceBase):
     for b in ret.buttonEvents:
       # do enable on both accel and decel buttons
       if b.type in [ButtonType.accelCruise, ButtonType.decelCruise] and not b.pressed:
-<<<<<<< HEAD
         events.add(EventName.buttonEnable, [ET.ENABLE]))
       # do disable on button down
       if b.type == ButtonType.cancel and b.pressed:
