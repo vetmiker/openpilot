4c4ad674b92458864a86e542893417685c877fdb
SCshredder17 <57152954+SCshredder17@users.noreply.github.com>
2020-07-24 10:53:14 -0700

    updated sparrowtuning
    additional reduced ping pong on curves, improved lane position on curves

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -260,15 +260,16 @@ class CarInterface(CarInterfaceBase):
       if spairrowtuning:
         ret.steerActuatorDelay = 0.52
         ret.steerRatio = 15.33
+        ret.steerLimitTimer = 2.0
         tire_stiffness_factor = 0.996  # not optimized yet
         #ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kpV = [[0.0, 15.5, 21.0, 29.0], [0.13, 0.39, 0.39, 0.6]]
         #ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kiV = [[0.0, 15.5, 21.0, 29.0], [0.005, 0.015, 0.015, 0.1]]
         #ret.lateralTuning.pid.kfBP, ret.lateralTuning.pid.kfV = [[0.0, 15.5, 21.0, 29.0], [0.00009, 0.00015, 0.00015, 0.00007818594]]
         ret.lateralTuning.init('indi')
-        ret.lateralTuning.indi.innerLoopGain = 6.5
+        ret.lateralTuning.indi.innerLoopGain = 6
         ret.lateralTuning.indi.outerLoopGain = 15.0
-        ret.lateralTuning.indi.timeConstant = 3.0
-        ret.lateralTuning.indi.actuatorEffectiveness = 4.5
+        ret.lateralTuning.indi.timeConstant = 5.5
+        ret.lateralTuning.indi.actuatorEffectiveness = 6.0
 
     elif candidate in [CAR.LEXUS_ES_TSS2, CAR.LEXUS_ESH_TSS2]:
       stop_and_go = True
