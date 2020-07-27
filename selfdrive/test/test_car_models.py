337582de92ae68f161589944f0ff30ca89fdf026
Arne Schwarck <arneschwarck@gmail.com>
2020-07-27 14:06:16 +0200

    Remove duplicate route

--- selfdrive/test/test_car_models.py
+++ selfdrive/test/test_car_models.py
@@ -341,10 +341,6 @@ routes = {
     'enableCamera': True,
     'enableDsu': False,
   },
-  "791340bc01ed993d|2019-03-10--16-28-08": {
-    'carFingerprint': SUBARU.IMPREZA,
-    'enableCamera': True,
-  },
   "76b83eb0245de90e|2019-10-20--15-42-29": {
     'carFingerprint': VOLKSWAGEN.GOLF,
     'enableCamera': True,
