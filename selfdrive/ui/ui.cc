06ff66527220cad9afaf8065b30085729e7c8c79
Arne Schwarck <arneschwarck@gmail.com>
2020-07-30 20:20:19 +0200

    refactor

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -363,10 +363,10 @@ void handle_message(UIState *s, SubMaster &sm) {
   if (sm.updated("radarState")) {
     auto data = sm["radarState"].getRadarState();
     scene.lead_data[0] = data.getLeadOne();
-    scene.lead_status = scene.lead_data[0].getStatus();
-    scene.lead_d_rel = scene.lead_data[0].getDRel();
+    scene.lead_status = data.getLeadOne().getStatus();
+    scene.lead_d_rel = data.getLeadOne().getDRel();
     //scene.lead_y_rel = scene.lead_data[0].getYRel();
-    scene.lead_v_rel = scene.lead_data[0].getVRel();
+    scene.lead_v_rel = data.getLeadOne().getVRel();
     scene.lead_data[1] = data.getLeadTwo();
     //scene.lead_status2 = scene.lead_data[1].getStatus;
     //scene.lead_d_rel2 = scene.lead_data[1].getDRel;
