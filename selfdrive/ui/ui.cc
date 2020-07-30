0f378a5d66361f6cec8b704778b09321fc4577a0
Arne Schwarck <arneschwarck@gmail.com>
2020-07-30 21:00:56 +0200

    remove IpAddr for now

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -423,7 +423,7 @@ void handle_message(UIState *s, SubMaster &sm) {
     scene.thermal = sm["thermal"].getThermal();
     scene.pa0 = scene.thermal.getPa0();
     scene.freeSpace = scene.thermal.getFreeSpace();
-    snprintf(scene.ipAddr, sizeof(scene.ipAddr), "%s", scene.thermal.getIpAddr());
+    //snprintf(scene.ipAddr, sizeof(scene.ipAddr), "%s", scene.thermal.getIpAddr());
   }
   if (sm.updated("ubloxGnss")) {
     auto data = sm["ubloxGnss"].getUbloxGnss();
