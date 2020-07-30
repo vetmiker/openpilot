251a627e9ca8cf265ed2a73ccb75fa639e8aeac5
Arne Schwarck <arneschwarck@gmail.com>
2020-07-30 08:04:40 +0200

    Let us see if this is a good idea

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -226,7 +226,7 @@ class Controls:
     if not self.sm.alive['plan'] and self.sm.alive['pathPlan']:
       # only plan not being received: radar not communicating
       self.events.add(EventName.radarCommIssue)
-    elif not self.sm.all_alive_and_valid():
+    elif not self.sm.all_alive_and_valid() and self.sm.frame > 10 / DT_CTRL:
       self.events.add(EventName.commIssue)
     if not self.sm['pathPlan'].mpcSolutionValid:
       self.events.add(EventName.plannerError)
