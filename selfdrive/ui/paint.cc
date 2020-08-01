65451e05c7f5219bfc87f781781d5ced8f632901
Arne Schwarck <arneschwarck@gmail.com>
2020-08-01 14:18:31 +0200

    fix undeclared scene

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -219,7 +219,7 @@ static void ui_draw_track(UIState *s, bool is_mpc, track_vertices_data *pvd) {
   if (is_mpc) {
     // Draw colored MPC track
     const uint8_t *clr = bg_colors[s->status];
-    if (scene->steerOverride) {
+    if (s->scene.steerOverride) {
       track_bg = nvgLinearGradient(s->vg, vwp_w, vwp_h, vwp_w, vwp_h*.4,
         nvgRGBA(0, 191, 255, 255), nvgRGBA(0, 95, 128, 50));
     } else {
