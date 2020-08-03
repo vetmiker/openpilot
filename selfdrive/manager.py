46af80afa1f6eea9c5d05127a1578c7ae8258c5f
Arne Schwarck <arneschwarck@gmail.com>
2020-08-03 07:28:41 +0200

    Fix typo

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -13,7 +13,7 @@ from typing import Dict, List
 from selfdrive.swaglog import cloudlog, add_logentries_handler
 from common.basedir import BASEDIR, PARAMS
 from common.android import ANDROID
-from common.op_param import opParams
+from common.op_params import opParams
 op_params = opParams()
 
 traffic_lights = op_params.get('traffic_lights', False)
