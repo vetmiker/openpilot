d273b8277711fd172f220494b50699482dec4b4e
Arne Schwarck <arneschwarck@gmail.com>
2020-08-09 20:11:56 +0200

    Add new events for keepHandsOnWheel

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -465,6 +465,30 @@ EVENTS = {
       AlertStatus.critical, AlertSize.full,
       Priority.HIGH, VisualAlert.steerRequired, AudibleAlert.chimeWarningRepeat, .1, .1, .1),
   },
+  
+  EventName.preKeepHandsOnWheel: {
+    ET.WARNING: Alert(
+      "HANDS OFF STEERING WHEEL: No Steering Pressed Detected",
+      "",
+      AlertStatus.normal, AlertSize.small,
+      Priority.LOW, VisualAlert.steerRequired, AudibleAlert.none, .0, .1, .1, alert_rate=0.75),
+  },
+
+  EventName.promptKeepHandsOnWheel: {
+    ET.WARNING: Alert(
+      "HANDS OFF STEERING WHEEL",
+      "Driver must keeP hands on wheel",
+      AlertStatus.userPrompt, AlertSize.mid,
+      Priority.MID, VisualAlert.steerRequired, AudibleAlert.chimeWarning2Repeat, .1, .1, .1),
+  },
+
+  EventName.keepHandsOnWheel: {
+    ET.WARNING: Alert(
+      "DISENGAGE IMMEDIATELY",
+      "Driver kept hands off sterring wheel",
+      AlertStatus.critical, AlertSize.full,
+      Priority.HIGH, VisualAlert.steerRequired, AudibleAlert.chimeWarningRepeat, .1, .1, .1),
+  },
 
   EventName.driverMonitorLowAcc: {
     ET.WARNING: Alert(
