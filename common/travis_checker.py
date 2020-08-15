8fe2904bb2a8708bf9eba18e78f67900255913b1
Shane Smiskol <shane@smiskol.com>
2020-08-15 02:15:02 -0500

    shorter

--- common/travis_checker.py
+++ common/travis_checker.py
@@ -1,2 +1,2 @@
 import os
-travis = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../")).strip('/').split('/')[0] != 'data'
+travis = os.path.dirname(os.getcwd()).strip('/').split('/')[0] != 'data'
