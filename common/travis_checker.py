06d77815b55ce7166da690b4794fd7ee55c3a3f7
Shane Smiskol <shane@smiskol.com>
2020-08-15 02:12:05 -0500

    Save a line

--- common/travis_checker.py
+++ common/travis_checker.py
@@ -1,3 +1,2 @@
 import os
-BASEDIR = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../"))
-travis = BASEDIR.strip('/').split('/')[0] != 'data'
+travis = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../")).strip('/').split('/')[0] != 'data'
