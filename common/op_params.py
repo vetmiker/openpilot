197df91017a63aff02cae0238061536603e0c1ee
Arne Schwarck <arneschwarck@gmail.com>
2020-08-18 09:58:43 +0200

    default to normal

--- common/op_params.py
+++ common/op_params.py
@@ -43,7 +43,7 @@ class opParams:
                            'curvature_factor': {'default': 1.2, 'allowed_types': [float, int], 'description': 'Multiplier for the curvature slowdown. Increase for less braking.', 'live': False},
                            'cloak': {'default': True, 'allowed_types': [bool], 'description': "make comma believe you are on their fork", 'live': False},
                            'default_brake_distance': {'default': 250.0, 'allowed_types': [float, int], 'description': 'Distance in m to start braking for mapped speeds.', 'live': False},
-                           'dynamic_follow': {'default': 'auto', 'allowed_types': [str],
+                           'dynamic_follow': {'default': 'normal', 'allowed_types': [str],
                                               'description': "Can be: ('close', 'normal', 'far'): Left to right increases in following distance.\n"
                                                              "All profiles support dynamic follow so you'll get your preferred distance while\n"
                                                              "retaining the smoothness and safety of dynamic follow!", 'live': True},
