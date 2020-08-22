617bc5ec15256233e0d83b3fadfafd4bbc1d0677
Arne Schwarck <arneschwarck@gmail.com>
2020-08-22 21:12:58 +0200

    enableCamera must be true for COROLLA_2015

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -352,7 +352,10 @@ class CarInterface(CarInterfaceBase):
     # mass and CG position, so all cars will have approximately similar dyn behaviors
     ret.tireStiffnessFront, ret.tireStiffnessRear = scale_tire_stiffness(ret.mass, ret.wheelbase, ret.centerToFront,
                                                                          tire_stiffness_factor=tire_stiffness_factor)
-    ret.enableCamera = is_ecu_disconnected(fingerprint[0], FINGERPRINTS, ECU_FINGERPRINT, candidate, Ecu.fwdCamera) or has_relay
+    if candidate == CAR.COROLLA_2015:
+      ret.enableCamera = True
+    else:
+      ret.enableCamera = is_ecu_disconnected(fingerprint[0], FINGERPRINTS, ECU_FINGERPRINT, candidate, Ecu.fwdCamera) or has_relay
     # Detect smartDSU, which intercepts ACC_CMD from the DSU allowing openpilot to send it
     smartDsu = 0x2FF in fingerprint[0]
     # In TSS2 cars the camera does long control
